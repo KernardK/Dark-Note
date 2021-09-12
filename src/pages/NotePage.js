f64f9ed2f1aa3019cde9c462168cfad0474295c1
KernardK <kernardk@gmail.com>
2021-09-13 01:29:37 +0200

    Added the Add, Edit and Delete functions

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
-function NotePage({ match }) {
+function NotePage({ match, history }) {
   let noteId = match.params.id;
 
   let [note, setNote] = useState(null);
@@ -14,10 +14,59 @@ function NotePage({ match }) {
   }, [noteId]);
 
   let getNote = async () => {
+    if (noteId === "new") return;
     let response = await fetch(`http://localhost:5000/notes/${noteId}`);
     let data = await response.json();
 
     setNote(data);
+  };
+
+  //Create Note
+  let createNote = async () => {
+    await fetch(`http://localhost:5000/notes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let updateNote = async () => {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  // Delete Note function
+  let deleteNote = async () => {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+    //redirects to the homepage
+    history.push("/");
+  };
+
+  //
+  let handleSubmit = () => {
+    //
+    if (noteId != "new" && !note.body) {
+      deleteNote();
+    } else if (noteId !== "new") {
+      updateNote();
+    } else if (noteId === "new" && note !== null) {
+      createNote();
+    }
+    // redirect the user to homepage
+    history.push("/");
   };
 
   // let note = notes.find((note) => note.id == noteId);
@@ -27,12 +76,25 @@ function NotePage({ match }) {
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
+
+        {/* Checks if the note is new to show delete button
+         if not change the button to the done button */}
+        {noteId !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
       </div>
 
-      <textarea value={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 }
