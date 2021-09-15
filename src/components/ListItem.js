dbdf8a2955f526d6b9ed2cb6e061a9d56f814588
KernardK <kernardk@gmail.com>
2021-09-15 19:59:01 +0200

    cleaned up the ListItem compnent for dispaying informtion

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Get title function
+let getTitle = (note) => {
+  // Split notes by lines
+  const title = note.body.split("\n")[0];
+
+  // make a list of all the lines then we will pull the first line
+  if (title.length > 45) {
+    return title.slice(0, 45);
+  }
+
+  return title;
+};
+
+// format date function
+let getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
+// preview of the note
+let getContent = (note) => {
+  // get the content after the title
+  let title = getTitle(note);
+  // removing the next line with an empty space
+  let content = note.body.replaceAll("\n", "");
+  // remove the title from the content
+  content = content.replaceAll(title, "");
+
+  // Checks the length of the text
+  if (content.length > 45) {
+    return content.slice(0, 45) + "...";
+  } else {
+    return content;
+  }
+};
+
 export const ListItem = ({ note }) => {
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note.body}</h3>
+        <h3>{getTitle(note)}</h3>
+        <p>
+          <span>{getTime(note)}</span>
+          {getContent(note)}
+        </p>
       </div>
     </Link>
   );
